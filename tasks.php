707c6d4e3c175d664aa5e845bf21fd6a9af5b74b
DIOS <joantiscar@gmail.com>
2018-09-20 21:24:26 +0200

    Fato vario

--- tasks.php
+++ tasks.php
@@ -5,7 +5,17 @@
  * Date: 20/09/18
  * Time: 19:39
  */
+//require 'models/Task.php';
+//
+//
+//new Task();
 require 'models/Task.php';
+$tasks = [
+    new Task('Comprar llet', false),
+    new Task('Comprar pa', true),
+    new Task('Comprar aigua', false),
+    new Task('Practicar PHP', true)
+];
+require 'views/tasks.blade.php';
 
 
-new Task();
